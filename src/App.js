483ccc3a8d420df69255218bccc009fb8747ccf1
manichodi <chodimani28@gmail.com>
2024-12-04 18:40:12 +0530

    backenduser

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { UserListComponent } from './components/UserListComponent';
+
+function App() {
+    return (
+        <div className="App">
+            <UserListComponent />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/UserListComponent.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Userlist } from "../userservice/UserList";
+export function UserListComponent() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        Userlist()
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div className='container'>
+            <h1>User List</h1>
+            <table className='table table-striped table-bordered'>
+
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
